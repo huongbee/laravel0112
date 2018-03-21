9a4fa44fc12c241162624a032fa1215e90fea4b4
Huong <you@example.com>
2018-03-21 18:57:12 +0700

    layout

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -1,4 +1,8 @@
 @extends('layout')
+@section('content')
 <h2>
     This is content of detail page
 </h2>
+@endsection
+
+@section('title','Chi tiết')
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layout')
-
+@section('content')
 <h2>
     This is content of Home Page
 </h2>
+<h3>....</h3>
+@endsection
+
+@section('title','Trang chủ')
